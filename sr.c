95b07b07d905de702809d108a7f4a31711a54ce4
Jimil <jimilpatel24@gmail.com>
2025-05-05 00:01:47 +0930

    Increased SEQSPACE to 12 to prevent buffer wrapping issues in SR

--- sr.c
+++ sr.c
@@ -27,7 +27,7 @@
 #define RTT  16.0       /* round trip time.  MUST BE SET TO 16.0 when submitting assignment */
 #define WINDOWSIZE 6    /* the maximum number of buffered unacked packet
                           MUST BE SET TO 6 when submitting assignment */
-#define SEQSPACE 7      /* the min sequence space for GBN must be at least windowsize + 1 */
+#define SEQSPACE 12      /* the min sequence space for GBN must be at least windowsize + 1 */
 #define NOTINUSE (-1)   /* used to fill header fields that are not being used */
 
 /* generic procedure to compute the checksum of a packet.  Used by both sender and receiver
