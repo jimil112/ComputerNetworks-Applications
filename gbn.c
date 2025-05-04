85ead6e1a18255c18aaf54754001d79e6d915f71
Jimil <jimilpatel24@gmail.com>
2025-05-04 23:56:53 +0930

    blunder

--- gbn.c
+++ gbn.c
@@ -25,7 +25,7 @@
 #define RTT  16.0       /* round trip time.  MUST BE SET TO 16.0 when submitting assignment */
 #define WINDOWSIZE 6    /* the maximum number of buffered unacked packet
                           MUST BE SET TO 6 when submitting assignment */
-#define SEQSPACE 12      /* the min sequence space for GBN must be at least windowsize + 1 */
+#define SEQSPACE 7      /* the min sequence space for GBN must be at least windowsize + 1 */
 #define NOTINUSE (-1)   /* used to fill header fields that are not being used */
 
 /* generic procedure to compute the checksum of a packet.  Used by both sender and receiver
@@ -57,11 +57,10 @@ bool IsCorrupted(struct pkt packet)
 
 /********* Sender (A) variables and functions ************/
 
-static struct pkt buffer[SEQSPACE];  /* array for storing packets waiting for ACK */
+static struct pkt buffer[WINDOWSIZE];  /* array for storing packets waiting for ACK */
 static int windowfirst, windowlast;    /* array indexes of the first/last packet awaiting ACK */
 static int windowcount;                /* the number of packets currently awaiting an ACK */
 static int A_nextseqnum;               /* the next sequence number to be used by the sender */
-static bool acked[SEQSPACE];           /* track which packets have been ACKed */
 
 /* called from layer 5 (application layer), passed the message to be sent to other side */
 void A_output(struct msg message)
@@ -82,8 +81,9 @@ void A_output(struct msg message)
     sendpkt.checksum = ComputeChecksum(sendpkt);
 
     /* put packet in window buffer */
-    buffer[A_nextseqnum % SEQSPACE] = sendpkt;
-    acked[A_nextseqnum % SEQSPACE] = false;
+    /* windowlast will always be 0 for alternating bit; but not for GoBackN */
+    windowlast = (windowlast + 1) % WINDOWSIZE;
+    buffer[windowlast] = sendpkt;
     windowcount++;
 
     /* send out packet */
@@ -121,12 +121,10 @@ void A_input(struct pkt packet)
       printf("----A: uncorrupted ACK %d is received\n",packet.acknum);
     total_ACKs_received++;
 
-    acked[packet.acknum % SEQSPACE] = true; 
-
     /* check if new ACK or duplicate */
     if (windowcount != 0) {
-          int seqfirst = buffer[windowfirst % SEQSPACE].seqnum;
-          int seqlast = buffer[(windowfirst + windowcount - 1) % SEQSPACE].seqnum;
+          int seqfirst = buffer[windowfirst].seqnum;
+          int seqlast = buffer[windowlast].seqnum;
           /* check case when seqnum has and hasn't wrapped */
           if (((seqfirst <= seqlast) && (packet.acknum >= seqfirst && packet.acknum <= seqlast)) ||
               ((seqfirst > seqlast) && (packet.acknum >= seqfirst || packet.acknum <= seqlast))) {
@@ -140,10 +138,10 @@ void A_input(struct pkt packet)
             if (packet.acknum >= seqfirst)
               ackcount = packet.acknum + 1 - seqfirst;
             else
-              ackcount = SEQSPACE - seqfirst + packet.acknum + 1;
+              ackcount = SEQSPACE - seqfirst + packet.acknum;
 
 	    /* slide window by the number of packets ACKed */
-            windowfirst = (windowfirst + ackcount) % SEQSPACE;
+            windowfirst = (windowfirst + ackcount) % WINDOWSIZE;
 
             /* delete the acked packets from window buffer */
             for (i=0; i<ackcount; i++)
@@ -174,10 +172,11 @@ void A_timerinterrupt(void)
     printf("----A: time out,resend packets!\n");
 
   for(i=0; i<windowcount; i++) {
-    if (TRACE > 0)
-      printf ("---A: resending packet %d\n", (buffer[(windowfirst+i) % SEQSPACE]).seqnum);
 
-    tolayer3(A,buffer[(windowfirst+i) % SEQSPACE]);
+    if (TRACE > 0)
+      printf ("---A: resending packet %d\n", (buffer[(windowfirst+i) % WINDOWSIZE]).seqnum);
+
+    tolayer3(A,buffer[(windowfirst+i) % WINDOWSIZE]);
     packets_resent++;
     if (i==0) starttimer(A,RTT);
   }
@@ -189,7 +188,6 @@ void A_timerinterrupt(void)
 /* entity A routines are called. You can use it to do any initialization */
 void A_init(void)
 {
-  int i;
   /* initialise A's window, buffer and sequence number */
   A_nextseqnum = 0;  /* A starts with seq num 0, do not change this */
   windowfirst = 0;
@@ -198,11 +196,8 @@ void A_init(void)
 		     so initially this is set to -1
 		   */
   windowcount = 0;
-
-  for (i = 0; i < SEQSPACE; i++) {
-    acked[i] = false;
-  }
 }
+
 
 
 /********* Receiver (B)  variables and procedures ************/
@@ -264,6 +259,10 @@ void B_init(void)
   expectedseqnum = 0;
   B_nextseqnum = 1;
 }
+
+/******************************************************************************
+ * The following functions need be completed only for bi-directional messages *
+ *****************************************************************************/
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
 void B_output(struct msg message)
